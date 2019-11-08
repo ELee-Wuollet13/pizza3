5bc33670e7c2701e660cab2253af03f077af70e5
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-11-08 13:01:45 -0800

    build price prototype- uncalled

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,27 @@
 //Backend Logic
 
 
-
-
-
-
-
-
-
-
-function Order(size, topping) {
+function order(size, topping, price) {
   this.size = size,
-  this.topping = topping
-  // this.price = price
+  this.topping = topping,
+  this.price = price
+  console.log("order");
 }
 
+order.prototype.setPrice = function() {
+  var price = 8;
+  console.log("price");
+  if ((this.size === xlrg) && (this.topping !== cheese)) {
+    return price += 8;
+    console.log(price);
+  } else if ((this.size === xlrg) && (this.topping === cheese) || (this.size === lrg) && (this.topping !== cheese)) {
+    return price + 6;
+  } else if ((this.size === lrg) && (this.topping === cheese) || (this.size === med) && (this.topping !== cheese)) {
+    return price + 3;
+  }   else {
+    return price;
+  }
+}
 
 //UI Logic
 $(document).ready(function() {
@@ -22,7 +29,7 @@ $(document).ready(function() {
     var size = $("#size").val()
     var topping = $("#topping").val()
     event.preventDefault();
-    var newOrder = new Order(size, topping)
+    var newOrder = new order(size, topping)
     console.log(newOrder);
   });
 });
